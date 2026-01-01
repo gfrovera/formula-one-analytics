e07d78f6da0014978cd95c89de286ce01ec1cccc
rileyrawr <gfrovera@gmail.com>
2025-12-31 17:45:46 -0800

    issue #1 - update db init to handle db standup

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,8 +1,25 @@
 from .base import Base
 from .session import engine
+from .session import get_session
 from . import models
 
 
 def init_db():
     '''Creates all tables that don't exist yet in database'''
     Base.metadata.create_all(engine)
+
+
+def get_data_loader():
+    '''Lazy import to avoid circular dependency'''
+    from .loaders import data_loader
+    return data_loader
+
+__all__ = [
+    'Base',
+    'engine',
+    'get_session',
+    'models',
+    'init_db',
+    'get_data_loader',
+]
+
